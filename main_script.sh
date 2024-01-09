718a8375dcdc7d77612d8afc80cb2ce35dec8014
gianvito21 <49394858+gianvito21@users.noreply.github.com>
2024-01-09 15:02:27 +0100

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -2,6 +2,10 @@ resourceGroup=$1
 aksName=$2
 token=$3
 
+echo $token
+echo $resourceGroup 
+echo $aksName
+
 curl -H 'Authorization: token $token'   -H 'Accept: application/vnd.github.v3.raw'   -O   -L https://raw.githubusercontent.com/gianvito21/deploymentsfile/main/script.txt
 mv script.txt script.sh
 chmod +x script.sh
