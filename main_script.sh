716de7e73c07a2278816c3a04bb435dbb0b4aed6
gianvito21 <49394858+gianvito21@users.noreply.github.com>
2024-01-10 10:22:55 +0100

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -6,15 +6,8 @@ echo $token
 echo $resourceGroup 
 echo $aksName
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
 curl -H "Authorization: token $token"   -H 'Accept: application/vnd.github.v3.raw'   -O   -L https://raw.githubusercontent.com/gianvito21/deploymentsfile/main/script.txt
-mv script.txt script.sh
-cat script.sh
-echo "separatoreeeee"
-ls
+mv script.txt script.s
 chmod +x script.sh
 ./script.sh $resourceGroup $aksName
 # . script.sh $resourceGroup $aksName
